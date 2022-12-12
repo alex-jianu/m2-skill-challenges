8330aa10ab88dbff64c4b16fdb78b00cc9b226f5
alex-jianu <alextj160398@gmail.com>
2022-12-12 16:28:42 +0000

    changes added

--- lib/grammer.rb
+++ lib/grammer.rb
@@ -2,6 +2,8 @@
 # So that I can improve my grammar
 # I want to verify that a text starts with a capital letter and ends with a suitable sentence-ending punctuation mark.
 
+#changes added by ALEX :)
+
 def grammer(text)
     if ['?','.','!'].include?(text[-1]) && text[0] == text[0].upcase
         return true
